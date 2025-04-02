d6f57ed083842be0b0bff0a38c2a812e08cacbf8
beckk3031 <beckk3031@student.faytechcc.edu>
2025-04-02 16:09:57 -0700

    Add files via upload
    First Program

---
+++ P1LAB1_BeckKyle.py
@@ -0,0 +1,13 @@
+#Kyle Beck
+#31 MAY 2025
+#P1LAB1
+#This is my first Python program.
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+#when trying to display the ! immediately after the input variable of last_name I recieved several syntax errors
+#I used the end variable with no space to ensure all text was on one line and the ! followed after last_name
+
+print('Hello,', first_name, last_name, end='')
+print('! Welcome to CTI-110')
